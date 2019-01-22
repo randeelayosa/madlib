ce52333b3e2a241e641e1d9ccd719af596d3298d
Randee Layosa and Slater Smith <slater@>
2019-01-22 12:02:10 -0800

    Add looping to refactor code.

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,29 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
-    var person1Input = $("input#person1").val();
-    var person2Input = $("input#person2").val();
-    var animalInput= $("input#animal").val();
-    var exclamationInput = $("input#exclamation").val();
-    var adjectiveInput = $("input#adjective").val();
-    var nounInput = $("input#noun").val();
+    var blanks = ["person1", "person2", "animal", "exclamation", "adjective", "noun"];
 
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".adjective").text(adjectiveInput);
-    $(".noun").text(nounInput);
+    blanks.forEach(function(blank) {
+      var userInput = $("input#" + blank).val();
+      $("." + blank).text(userInput);
+    });
+
+
+
+    //was reduced as seen on line 3
+    // var person1Input = $("input#person1").val();
+    // var person2Input = $("input#person2").val();
+    // var animalInput= $("input#animal").val();
+    // var exclamationInput = $("input#exclamation").val();
+    // var adjectiveInput = $("input#adjective").val();
+    // var nounInput = $("input#noun").val();
+    //
+    //was reduced as seen on line 5
+    // $(".person1").text(person1Input);
+    // $(".person2").text(person2Input);
+    // $(".animal").text(animalInput);
+    // $(".exclamation").text(exclamationInput);
+    // $(".adjective").text(adjectiveInput);
+    // $(".noun").text(nounInput);
 
     $("#story").show();
 
